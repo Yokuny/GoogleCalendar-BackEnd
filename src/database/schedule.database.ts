0bcef7a990e541767a41c0dd2741598d736d7613
Felipe Rangel Ribeiro <felipe.vni@hotmail.com>
2025-03-09 15:01:33 -0300

    fix: env adjusts

--- src/database/schedule.database.ts
+++ src/database/schedule.database.ts
@@ -1,23 +1,11 @@
 import mongoose from "mongoose";
 
-const scheduleSchema = new mongoose.Schema(
-  {
-    User: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-    description: { type: String, required: true },
-    startTime: { type: Date, required: true },
-    endTime: { type: Date },
-    createdAt: { type: Date, default: Date.now },
-  },
-  {
-    toJSON: {
-      transform: function (doc, ret) {
-        if (ret.createdAt) ret.createdAt = ret.createdAt.toISOString();
-        if (ret.startTime) ret.startTime = ret.startTime.toISOString();
-        if (ret.endTime) ret.endTime = ret.endTime.toISOString();
-        return ret;
-      },
-    },
-  }
-);
+const scheduleSchema = new mongoose.Schema({
+  User: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  description: { type: String, required: true },
+  startTime: { type: Date, required: true },
+  endTime: { type: Date },
+  createdAt: { type: Date, default: Date.now },
+});
 
 export const Schedule = mongoose.model("Schedule", scheduleSchema);
